76c0f450b0b4951f95f6b9abac9eeece654a24b6
muluken bogale <mulukenb89@gmail.com>
2019-02-13 16:49:10 +0300

    adding three method in MainActivity for onCLick methods for buttons

--- app/src/main/java/com/example/simplestopwatch/MainActivity.java
+++ app/src/main/java/com/example/simplestopwatch/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.simplestopwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int second;  //the amount of second passed
+    private boolean running;  //check whether time is running or not
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startTime(View view){
+        running = true;
+
+    }
+    public void stopTime(View view){
+        running = false;
+
+    }
+    public void resetTime(View view){
+        running = false;
+        second = 0;
+
     }
 }
